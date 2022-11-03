9d35a86bda6996173153e0d1a86f60e5d8e7bbed
Sharadha1 <117355634+Sharadha1@users.noreply.github.com>
2022-11-03 21:45:31 +1100

    Add files via upload

---
+++ Deaths of Indigenous Australians per 100000 persons between 2011 and 2015.R
@@ -0,0 +1,44 @@
+install.packages("blogdown")
+
+library(blogdown)
+
+library(rmarkdown)
+install.packages("viridis")
+library("viridis")
+install.packages("extrafont")
+library(extrafont)
+install.packages("tidyverse")
+library(tidyverse)
+IndigenousDeaths <- read.csv('C:/Users/shara/OneDrive/Desktop/COMM2501 - R labs/R Blogdown csv file.csv')
+
+Disease_Name = c("Circulatory diseases", "Neoplasms", "External Causes", "Endocrine, Metabolic & Nutritional Disorders","Diabetes", "Respiratory Diseases", "Digestive Diseases", "Nervous System Diseases", "Infectious and Parasitic Diseases", "Kidney Diseases", "Conditions from Perinatal Period", "Other")
+
+Deaths_per_100000_persons = c(271.4,232.1,81.3,100.6,87.3,100.6,46,26.1,20.8,24,4.7,84.1)
+
+IndigenousDeathsData <- data.frame(Disease_Name,Deaths_per_100000_persons)
+
+
+my_ggplot <- ggplot(
+  data=IndigenousDeathsData, aes(
+  x=Disease_Name, y=Deaths_per_100000_persons, fill=Disease_Name) 
+) + 
+ geom_bar(stat="identity", width =0.5) + 
+ geom_text(aes(label=Deaths_per_100000_persons), vjust= -0.3, size=3.5) + 
+ theme_minimal() + 
+ ggtitle (" Age Standardised Deaths of Indigenous Australians per 100000 persons between 2011 & 2015") + 
+  xlab("Disease Name") +
+  ylab(" Deaths per 100000 persons")+
+  guides(fill=guide_legend(title = "Disease Name"))+
+ scale_fill_viridis(discrete = TRUE, option ="magma") +
+ ylim(0,300) + 
+ theme(axis.text.x = element_text(angle = 60, hjust=1,size =12), 
+       axis.text.y = element_text(hjust=1, size =12),
+       axis.title.y = element_text(hjust=0.5, size = 14, margin=margin(t=0, r=20, b=0, l=0)),
+       axis.title.x = element_text (hjust=0.5, size = 14),
+       plot.title = element_text(hjust=0.5, size=25))
+my_ggplot
+      
+      
+
+
+        
